0d967e1485a216b75f471f451b7abf9e7f88c96b
Sicarde <camille.koziar@gmail.com>
2016-05-03 06:35:31 +0900

    Saving improved
    Problem with variables set in the loaded scene (they get [de/in]creased
    once before saved and once again when the scene is loaded), no multiple
    slots yet

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
@@ -7,28 +7,67 @@ using UnityTwine;
 
 public class PauseMenuScript : MonoBehaviour {
 	public GameObject pauseMenu;
-	public TwineTextPlayer twineTextPlayer;
+	public Story storyScript;
+	string[] vars;
 
-	public void Start()
-	{
-		Debug.Log("Load: " + PlayerPrefs.GetString ("SavedLevel"));
+	void Start() {
+		vars = new string[] {
+			"fail",
+			"metFriend",
+			"morals",
+			"sociability",
+			"suspicion"
+		};
+		Resume();
 	}
 
-	public void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.P))
-		{
-			//this.gameObject.SetActive(true);
+	public void Load(string passageName) {
+		if (passageName == "") {
+			passageName = PlayerPrefs.GetString ("SavedLevel");
+		}
+		Debug.Log("Load: " + passageName);
+		string paramsValue = PlayerPrefs.GetString("paramsValue");
+		string[] paramsValues = paramsValue.Split('|');
+		long i = 0;
+		foreach (string pValue in paramsValues) {
+			TwineVar var = new TwineVar(pValue);
+			storyScript[vars[i]] = var;
+			i++;
+		}
+		storyScript.GoTo(passageName);
+		Resume();
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.P)) {
+			if (pauseMenu.activeSelf == false) {
+				pauseMenu.SetActive(true);
+			} else {
+				Resume();
+			}
 		}
 	}
 
-	public void Save() {
-		TwineStory story = twineTextPlayer.Story;// GetComponentsInChildren<Story> ();
-		Debug.Log("Save: " + story.CurrentPassageName);
-		PlayerPrefs.SetString("SavedLevel",story.CurrentPassageName);
+	void addParamToString(ref string paramValue, string param) {
+		if (paramValue.Length > 0) {
+			paramValue += "|";
+		}
+		paramValue += storyScript[param].ToInt().ToString();
 	}
+
+	public void Save() {
+		Debug.Log("Save: " + storyScript.CurrentPassageName);
+		PlayerPrefs.SetString("SavedLevel", storyScript.CurrentPassageName);
+		string paramsValue = "";
+		foreach (string var in vars) {
+			addParamToString(ref paramsValue, var);
+		}
+		Debug.Log(paramsValue);
+		PlayerPrefs.SetString("paramsValue", paramsValue);
+	}
+
 	public void Resume() {
 		Debug.Log("Resume");
-		this.gameObject.SetActive(false);
+		pauseMenu.SetActive(false);
 	}
 }
